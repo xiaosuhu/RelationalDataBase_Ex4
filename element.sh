0c89d293c28fc9d2d4b321b3a69ee9588b865443
xiaosuhu <xiaosuhu@outlook.com>
2023-02-28 16:17:22 +0000

    refactor: reformat the SQL query output

--- element.sh
+++ element.sh
@@ -3,3 +3,6 @@
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 echo "Please provide an element as an argument."
+read ELEMENT
+ELEMENT_INFO=$($PSQL "select * from elements inner join properties using(atomic_number) inner join types using (type_id) where atomic_number=$ELEMENT or symbol='$ELEMENT' or name='$ELEMENT'")
+echo "$ELEMENT_INFO"
