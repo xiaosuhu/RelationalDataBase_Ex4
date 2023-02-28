916fc6615e9ac1f650cb1817c1a626354e4e3fe8
xiaosuhu <xiaosuhu@outlook.com>
2023-02-28 16:40:46 +0000

    fix: add regular ex to switch number and string

--- element.sh
+++ element.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-#!/bin/bash
-
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 echo "Please provide an element as an argument."
 read ELEMENT
-ELEMENT_INFO=$($PSQL "select * from elements inner join properties using(atomic_number) inner join types using (type_id) where symbol='$ELEMENT' or name='$ELEMENT'")
+
+# switch between number and string
+re='^[0-9]+$'
+if [[ $ELEMENT =~ $re ]]
+then
+   ELEMENT_INFO=$($PSQL "select * from elements inner join properties using(atomic_number) inner join types using (type_id) where atomic_number=$ELEMENT")
+else
+  ELEMENT_INFO=$($PSQL "select * from elements inner join properties using(atomic_number) inner join types using (type_id) where symbol='$ELEMENT' or name='$ELEMENT'")
+fi
 echo "$ELEMENT_INFO"
