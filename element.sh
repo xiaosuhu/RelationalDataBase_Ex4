89d0a7846d239b913076b5067e791af0aba1f241
xiaosuhu <xiaosuhu@outlook.com>
2023-02-28 16:22:07 +0000

    chore: shebang

--- element.sh
+++ element.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+#!/bin/bash
+
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 echo "Please provide an element as an argument."
 read ELEMENT
-ELEMENT_INFO=$($PSQL "select * from elements inner join properties using(atomic_number) inner join types using (type_id) where atomic_number=$ELEMENT or symbol='$ELEMENT' or name='$ELEMENT'")
+ELEMENT_INFO=$($PSQL "select * from elements inner join properties using(atomic_number) inner join types using (type_id) where symbol='$ELEMENT' or name='$ELEMENT'")
 echo "$ELEMENT_INFO"
