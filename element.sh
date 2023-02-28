17926dbd7fd662ebed5d8c1b3fe08347e1ad4e61
xiaosuhu <xiaosuhu@outlook.com>
2023-02-28 17:21:02 +0000

    fix: the while reading variable

--- element.sh
+++ element.sh
@@ -2,17 +2,26 @@
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-echo "Please provide an element as an argument." 
-
 #read ELEMENT
 ELEMENT=$1
+
+if [[ -z $ELEMENT ]]
+then
+  echo "Please provide an element as an argument." 
+fi
 
 # switch between number and string
 re='^[0-9]+$'
 if [[ $ELEMENT =~ $re ]]
 then
-   ELEMENT_INFO=$($PSQL "select * from elements inner join properties using(atomic_number) inner join types using (type_id) where atomic_number=$ELEMENT")
+  ELEMENT_INFO=$($PSQL "select atomic_number, symbol, name, atomic_mass,melting_point_celsius,boiling_point_celsius, type from elements inner join properties using(atomic_number) inner join types using (type_id) where atomic_number=$ELEMENT")
 else
-  ELEMENT_INFO=$($PSQL "select * from elements inner join properties using(atomic_number) inner join types using (type_id) where symbol='$ELEMENT' or name='$ELEMENT'")
+  ELEMENT_INFO=$($PSQL "select atomic_number, symbol, name, atomic_mass,melting_point_celsius,boiling_point_celsius, type from elements inner join properties using(atomic_number) inner join types using (type_id) where symbol='$ELEMENT' or name='$ELEMENT'")
 fi
-echo "$ELEMENT_INFO"
+
+# Parse the output
+echo "$ELEMENT_INFO" | while read ATOMIC_NUMBER BAR SYMBOL BAR NAME BAR ATOMIC_MASS BAR MELTING BAR BOILING BAR TYPE
+do 
+  #echo "$SYMBOL"
+  echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+done
